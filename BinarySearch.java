e349f7a7d7491afab7084c2eada8e0922ebb143b
shivomm4887-coder <shivomm4887@gmail.com>
2026-03-07 16:07:26 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -42,10 +42,11 @@ public class BinarySearch {
         if (result != -1) {
             System.out.println("Element found at index: " + result);
         } else {
-            System.out.println("Element not found in the array.");
+            System.out.println("Element not found in the array");
         }
         
         scanner.close();
     }
 }
+
 
